442fd9912894001aca14f0d03857106a1168c1be
Mich21050 <michael.wanninger@pm.me>
2024-05-04 20:31:27 +0200

    Add files via upload

--- kiosk-installer-test.sh
+++ kiosk-installer-test.sh
@@ -62,7 +62,25 @@ cat > /home/kiosk-user/.config/openbox/autostart << EOF
 #!/bin/bash
 
 unclutter -idle 0.1 -grab -root &
-source /home/kiosk-user/chromeKiosk/start.sh &
+
+value=`cat /home/kiosk-user/chromeKiosk/url.txt`
+
+chromium \
+    --no-first-run \
+    --ignore-certificate-errors \
+    --disable-restore-session-state \
+    --start-maximized \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-save-password-bubble \
+    --disable-session-crashed-bubble \
+    --incognito \
+    --kiosk \
+    --remote-debugging-port=9222 \
+    --remote-allow-origins=* \
+    $value
 EOF
 
 echo "Done!"
