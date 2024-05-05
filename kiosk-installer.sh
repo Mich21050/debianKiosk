4864d2de375bb0af7422fc8bacfb87da0c31ff67
Mich21050 <michael.wanninger@pm.me>
2024-05-05 20:30:19 +0200

    Add files via upload

--- kiosk-installer.sh
+++ kiosk-installer.sh
@@ -72,41 +72,7 @@ fi
 wget -O /home/kiosk-user/.config/openbox/autostart https://raw.githubusercontent.com/Mich21050/debianKiosk/main/start.sh
 chown -R kiosk-user /home/kiosk-user/
 
-cat > /home/kiosk-user/getimage.sh << EOF
-#!/bin/bash
-
-# Set the name of the Docker image
-image_name="mich21050/chromekiosk"
-
-# Set the number of retries
-num_retries=10
-
-# Function to check if the Docker image exists
-image_exists() {
-    docker image inspect "$image_name" &> /dev/null
-}
-
-# Function to pull the Docker image
-pull_image() {
-    echo "Pulling Docker image: $image_name"
-    docker pull "$image_name"
-}
-
-# Main loop to check if the image exists and pull if necessary
-for (( i=1; i<=$num_retries; i++ )); do
-    if image_exists; then
-        echo "Docker image $image_name exists."
-        break
-    else
-        echo "Docker image $image_name not found. Attempt $i of $num_retries"
-        if [ $i -eq $num_retries ]; then
-            echo "Max retries reached. Exiting..."
-            exit 1
-        fi
-        pull_image
-    fi
-done
-EOF
+wget -O /home/kiosk-user/getimage.sh https://raw.githubusercontent.com/Mich21050/debianKiosk/main/getimage.sh
 chmod +x /home/kiosk-user/getimage.sh
 
 # docker run -d --network=host --restart=always -v /home/kiosk-user/chromeKiosk:/code/config --name=chromeKiosk mich21050/chromekiosk
