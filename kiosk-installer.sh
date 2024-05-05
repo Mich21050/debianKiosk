7eb1e6119018d431d1f53ec92a4ee635b3983c65
Mich21050 <michael.wanninger@pm.me>
2024-05-05 15:11:25 +0200

    Add files via upload

--- kiosk-installer.sh
+++ kiosk-installer.sh
@@ -16,7 +16,16 @@ apt-get install \
     sudo \
     -y
 
-wget -O - https://get.docker.com/ | bash
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # dir
 mkdir -p /home/kiosk-user/.config/openbox
 mkdir -p /home/kiosk-user/chromeKiosk/
