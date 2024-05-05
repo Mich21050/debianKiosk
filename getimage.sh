73c7304b29af51d8a7669b7dd6b9acd7f6801074
Mich21050 <michael.wanninger@pm.me>
2024-05-05 21:56:52 +0200

    Update getimage.sh

--- getimage.sh
+++ getimage.sh
@@ -3,9 +3,6 @@
 # Set the name of the Docker image
 image_name="mich21050/chromekiosk"
 
-# Set the number of retries
-num_retries=10
-
 # Function to check if the Docker image exists
 image_exists() {
     docker image inspect "$image_name" &> /dev/null
@@ -17,17 +14,20 @@ pull_image() {
     docker pull "$image_name"
 }
 
-# Main loop to check if the image exists and pull if necessary
-for (( i=1; i<=$num_retries; i++ )); do
-    if image_exists; then
-        echo "Docker image $image_name exists."
-        break
-    else
-        echo "Docker image $image_name not found. Attempt $i of $num_retries"
-        if [ $i -eq $num_retries ]; then
-            echo "Max retries reached. Exiting..."
-            exit 1
-        fi
-        pull_image
-    fi
-done
+# Check if the image exists
+if ! image_exists; then
+    echo "Docker image $image_name not found."
+
+    # Check for internet connectivity
+    echo "Checking internet connectivity..."
+    while ! ping -c 1 -W 3 google.com &> /dev/null; do
+        echo "Internet not reachable. Retrying in 5 seconds..."
+        sleep 5
+    done
+    echo "Internet is reachable."
+
+    # Pull the image
+    pull_image
+else
+    echo "Docker image $image_name exists. No action needed."
+fi
